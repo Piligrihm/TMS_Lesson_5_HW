ce55a40ae50d4532e6a737fac7c6b298502844a1
Piligrihm <71782601+Piligrihm@users.noreply.github.com>
2021-09-01 14:33:55 +0300

    Add files via upload

--- src/com/tms/array/Task_05.java
+++ src/com/tms/array/Task_05.java
@@ -1,4 +1,31 @@
 package com.tms.array;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите жеалемый размер массива: ");
+        try {
+            int sizeArray = scanner.nextInt();
+            int[] myArray = new int[sizeArray];
+
+            for (int i = 0; i < myArray.length; i++) {
+                myArray[i] = (int) (1000 * Math.random());
+            }
+            System.out.println("Сгенерированный массив: " + Arrays.toString(myArray));
+            for (int i = 0; i < myArray.length; i++) {
+                if (i % 2 != 0) {
+                    myArray[i] = 0;
+                }
+            }
+            System.out.println("Полученный массив: " + Arrays.toString(myArray));
+        } catch (InputMismatchException e) {
+            System.out.println("Введенное занчение ошибочно!");
+        } finally {
+            scanner.close();
+        }
+    }
 }
--- src/com/tms/array/Task_06.java
+++ src/com/tms/array/Task_06.java
@@ -1,3 +1,12 @@
+package com.tms.array;
+
+import java.util.Arrays;
 
 public class Task_06 {
+    public static void main(String[] args) {
+        String[] names = {"Саша", "Дима", "Андрей", "Анастасия", "Даша", "Олег", "Диана", "Максим"};
+        System.out.println("Заданный массив до сортировки: " + Arrays.toString(names));
+        Arrays.sort(names);
+        System.out.println("Заданный после сортировки: " + Arrays.toString(names));
+    }
 }
